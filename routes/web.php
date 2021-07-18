3b12744d93c27996caf40f669e78e21643e2a09d
Manuel <adrian-diaz200897@outlook.com>
2021-07-18 01:27:40 -0500

    Proyecto segundo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('estudiante','EstudianteController@index');
+#get -post - put - delete -show
+
+Route::get('dashboard','DashboardController@index') -> name('dashboard.index');
+
+Route::get('/tarea/buscar','TareaController@buscar');
+
+Route::get('saludo', function(){
+    return "bienvenidos: hello world ";
+});
+Route::get('/post/{numero?}', function($numero=1){
+    return "el numero: ".$numero;
+})->where('numero','[0-9]+');
+
+Route::get('/obra/{genero}',function($genero){
+    switch ($genero) {
+        case 'amor':
+            return "la obra es de amor";
+            break;
+        case 'accion':
+            return "esta obra es de accion";
+            break;
+        default:
+            return "esta obra no esta en la colección";
+            break;
+    }
 });
